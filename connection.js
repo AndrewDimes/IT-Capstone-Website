73e320ebeadc92d4bcba2111c90a1b10cdcb2ea1
Andrew Dimes <andrewdimes@gmail.com>
2022-11-23 19:17:41 -0800

    Fix

--- connection.js
+++ connection.js
@@ -1,20 +1,23 @@
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'b990ec48f77a5d',
     password: '14d81dbb',
     database: 'heroku_52166e7e22738e2',
   });
   
-  mysqlConnection.connect((error) => {
-    if(error){
-      console.log('Error connecting to the MySQL Database');
-      return;
-    }
-    console.log('Connection established sucessfully');
+// Attempt to catch disconnects 
+mysqlConnection.on('connection', function (connection) {
+  console.log('DB Connection established');
+
+  mysqlConnection.on('error', function (err) {
+    console.error(new Date(), 'MySQL error', err.code);
   });
-  //mysqlConnection.end((error) => {
-  //});
+  mysqlConnection.on('close', function (err) {
+    console.error(new Date(), 'MySQL close', err);
+  });
+})
+
 
   module.exports = mysqlConnection;
