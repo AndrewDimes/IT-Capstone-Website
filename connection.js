4a66a05639790c16cdbb42666ce95f688ddded8a
Andrew Dimes <andrewdimes@gmail.com>
2022-11-23 18:09:31 -0800

    db fix

--- connection.js
+++ connection.js
@@ -1,19 +1,26 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
-const mysqlConnection = mysql.createConnection({
-    host: '34.171.255.38',
-    user: 'root',
-    database: 'itcapstone',
-  });
-  
-  mysqlConnection.connect((error) => {
-    if(error){
-      console.log('Error connecting to the MySQL Database');
-      return;
-    }
-    console.log('Connection established sucessfully');
-  });
-  //mysqlConnection.end((error) => {
-  //});
+// Sets the right connection details for the connection string available
+const connectionDetails = () => {
+  // Check if heroku CLEARDB_DATABASE_URL is available
+  if (process.env.CLEARDB_DATABASE_URL) { return `${process.env.CLEARDB_DATABASE_URL}`; }
+  return {
+    database: `${process.env.DB}`,
+    host: `${process.env.DB_HOST}`,
+    user: `${process.env.DB_USER}`,
+    password: `${process.env.DB_USER_PASSWORD}`,
+  };
+};
+
+// Create a connection to the database
+const connection = mysql.createConnection(connectionDetails());
+
+// open the MySQL connection
+connection.connect((error) => {
+  if (error) throw error;
+  // eslint-disable-next-line no-console
+  console.log(`Successfully connected to the database with ${connectionDetails()}.`);
+});
 
   module.exports = mysqlConnection;
